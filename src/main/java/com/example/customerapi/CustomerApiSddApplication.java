5fabb45508791f9b9952c69208610e2c3af1272b
Erik Marques <marques.erik@gmail.com>
2026-09-22 20:37:21 -0300

    chore: initialize customer api

---
+++ src/main/java/com/example/customerapi/CustomerApiSddApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiSddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiSddApplication.class, args);
+	}
+
+}
